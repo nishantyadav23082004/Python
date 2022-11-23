72a2f0df08ef5b23b6cd03fa928c90f5073b8e86
nishantyadav23082004 <113028102+nishantyadav23082004@users.noreply.github.com>
2022-11-23 11:07:32 +0530

    Add files via upload
    This is a python program to make you familiar with f.readline() in file handling
    File Handling Basics - 2

---
+++ readline.py
@@ -0,0 +1,5 @@
+# This opens the file demofile.txt in read mode
+f = open("C:\\Python\demofile.txt","r")
+print(f.readline()) # This prints the first line of demofile.txt
+
+print(f.readline()) # This prints the second line of demofile.txt
